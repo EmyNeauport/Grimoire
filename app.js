5980c72cc2b7e9d9a4ff346a29bd7f6bac8e1642
EmyNeauport <emilie.neauport@outlook.fr>
2025-03-02 16:30:08 +0100

    step 1.2

--- app.js
+++ app.js
@@ -32,7 +32,26 @@ app.post ('/api/books',(req, res, next) => {
     .catch(error => res.status(400).json({error}));
 });
 
+app.put('/api/books/:id',(req, res, next) => {
+    Book.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Objet modifié!!'}))
+    .catch(error => res.status(404).json({error}));
+})
+
+app.delete('/api/books/:id',(req, res, next) => {
+    Book.deleteOne({ _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Objet supprimé!!!'}))
+    .catch(error => res.status(404).json({error}));
+})
+
+app.get('/api/books/:id',(req, res, next) => {
+    Book.findOne({ _id: req.params.id})
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({error}));
+})
+
 app.get('/api/books',(req, res, next) => {
+    //retourner la liste complète des livres en BDD
     Book.find()
     .then(books => res.status(200).json(books))
     .catch(error => res.status(400).json({error}));
